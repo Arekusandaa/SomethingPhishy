89843460e8352742e3ae39cd41f2c276901e38d0
Arekusandaa <alittaua67@gmail.com>
2017-09-30 09:18:25 -0700

    Added comments to WandController

--- SomethingPhishy/Assets/WandController.cs
+++ SomethingPhishy/Assets/WandController.cs
@@ -15,16 +15,25 @@ public class WandController : MonoBehaviour {
     public bool triggerButtonUp = false;
     public bool triggerButtonPressed = false;
 
+
+
+    //Creates a read only value for the tracked object to determine what controller is at which index
+    //In layman's speak, what controller is in what hand.
+    //The trackObj is the variable that determines which object is being tracked now.
     private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
     private SteamVR_TrackedObject trackedObj;
 
     // Use this for initialization
+    //Begins script by retrieving the trackedObj component and get index values
     void Start()
     {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
     }
 
     // Update is called once per frame
+    //This function is an "always-listening" function
+    //This one determines whether a grip button or trigger was pressed and unpressed
+    //Variables and methods are self-explanatory
     void Update()
     {
         if (controller == null)
